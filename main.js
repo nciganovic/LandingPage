95b76b220a5d79533585c5128385b833d736de21
nciganovic <nciganovic99@gmail.com>
2020-01-13 00:43:41 +0100

    impoving website

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 var nav = document.getElementsByTagName('nav')[0];
+if(window.scrollY > 5){
+    nav.classList.remove('transp-nav');
+    nav.classList.add('dark-nav');
+}
+else{
+    nav.classList.add('transp-nav');
+    nav.classList.remove('dark-nav');
+}
 window.addEventListener('scroll', function(e){
     //console.log(window.scrollY);
     if(window.scrollY > 5){
